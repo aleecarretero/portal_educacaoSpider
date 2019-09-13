557190e330100b5e645c5080a3f0aec18ce9d910
Alexandre Carretero <alexandre@omie.com.br>
2019-09-13 12:42:19 -0300

    second jQuery call fix
    Now the crawler will search for all of he courses and append the result to a file

--- portal_educacaoSpider.js
+++ portal_educacaoSpider.js
@@ -11,18 +11,19 @@ request('https://www.portaleducacao.com.br/cursos?utm_campaign=cursos&utm_medium
 
         var title = $(this).find('h2').text().trim().toUpperCase();
 
-        console.log(title);
+        // console.log(title);
 
-        var list = cheerio.load(list(this).html());
+        $('ul li.cursos-online-de-informatica-e-tecnologia---portal-educacao').each(function(){
 
-        list('ul li.cursos-online-de-informatica-e-tecnologia---portal-educacao').each(function(){
+            var categoria = $(this).find('div.categoriaProduto').text().trim();
+            var nome = $(this).find('div.nomeProduto').text().trim();
+            var cargaHoraria = $(this).find('div.cargaHoraria').text().trim();
+            var precoDe = $(this).find('div.precoDe').text().trim();
+            var precoAtual = $(this).find('div.precoPor').text().trim();
 
-            var categoria = list(this).html().find('div.categoriaProduto').text().trim();
-            var nome = list(this).html().find('div.nomeProduto').text().trim();
-            var cargaHoraria = list(this).html().find('div.cargaHoraria').text().trim();
-            var precoDe = list(this).html().find('div.precoDe').text().trim();
-            var precoAtual = list(this).html().find('div.precoPor').text().trim();
-
+            fs.appendFile('extract.txt', title + '\n'+ categoria + '\n' + nome + '\n' + cargaHoraria+ '\n' +precoDe+ '\n' +precoAtual+ '\n-----------------\n', (err) => {
+                if (err) console.log(err);
+            });
         });
     });
 });
